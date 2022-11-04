7d14d1a92a04c4a03070d7947981eaea3ef49a44
zakzakst <e1433104@gmail.com>
2022-11-04 15:19:57 +0900

    3-23

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  meta: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+      }
+    ]
+  }
 })
