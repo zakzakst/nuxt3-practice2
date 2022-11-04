9ab200f4bb740a5c4189caf1aa19d9dc7587a3e6
zakzakst <e1433104@gmail.com>
2022-11-04 10:23:14 +0900

    2-18

--- components/Option.vue
+++ components/Option.vue
@@ -15,6 +15,16 @@ interface OptionProps {
 }
 
 const props = defineProps<OptionProps>();
+
+const computeButtonClasses = (value, index) => {
+  const classNames = [];
+  if (props.options[props.option.category] === value) {
+    classNames.push('option-active');
+  }
+  if (index === 0) classNames.push('option-left');
+  if (index === props.option.buttons.length - 1) classNames.push('option-right');
+  return classNames.join(' ');
+};
 </script>
 
 <template>
@@ -22,10 +32,10 @@ const props = defineProps<OptionProps>();
     <h4>{{ option.title }}</h4>
     <div class="option-buttons">
       <button
-        v-for="value in option.buttons"
+        v-for="(value, index) in option.buttons"
         :key="value"
-        class="option option-left"
-        :class="options[option.category] === value && 'option-active'"
+        class="option"
+        :class="computeButtonClasses(value, index)"
         @click="options[option.category] = value"
       >
         {{ value }}
