945f7bdc6a85ef550517cff06698b268d244a490
zakzakst <e1433104@gmail.com>
2022-11-04 10:35:19 +0900

    2-19

---
+++ components/Card/Name.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+interface NameProps {
+  name: string;
+}
+
+const props = defineProps<NameProps>();
+</script>
+
+<template>
+  <div class="card">
+    <h4>{{ name }}</h4>
+    <p>x</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  border-radius: 1rem;
+  padding: 0.1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+.card p {
+  position: absolute;
+  top: -29%;
+  left: 92.5%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
+}
+</style>
